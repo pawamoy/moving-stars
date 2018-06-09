6e335bc2763ba96e0ae11b08fd1e4cac99f77984
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2018-06-09 16:53:36 +0200

    Fix mapping data is sorted job

--- sort_data.sh
+++ sort_data.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sort -u mapping_data.toml | tail -n+3 > /tmp/moving_stars_mapping_data || exit 1
+unset LANG
+sort -u mapping_data.toml | head -n-2 > /tmp/moving_stars_mapping_data || exit 1
 {
   echo data = [
   cat /tmp/moving_stars_mapping_data
